cb9756b4ab36919a4f081103f0cf352b5f23b09b
dangelo <dangelomedinag@gmail.com>
2021-01-24 13:47:52 -0300

    thrid commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
+  import { onMount } from 'svelte';
+
   let name = 'world';
   let counter = 1;
+  onMount(() => {
+    console.log('component Mount');
+  });
 </script>
 
 <main>
@@ -9,12 +14,31 @@
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
     how to build Svelte apps.
   </p>
-  <button on:click={() => counter--}>-</button><span style="padding: 0 1em"
-    >{counter}</span
-  ><button on:click={() => counter++}>+</button>
+  <button on:click={() => counter--} disabled={counter <= 0}>-</button>
+  {#key counter}
+    <span style="padding: 0 1em">{counter}</span>
+  {/key}
+  <button on:click={() => counter++} disabled={counter >= 10}>+</button>
 </main>
 
 <style>
+  span {
+    display: inline-block;
+    width: 80px;
+  }
+
+  button {
+    display: inline-block;
+    padding: 0 1em;
+  }
+
+  button:disabled {
+    opacity: 0.3;
+  }
+
+  button:hover {
+    background: #f2f2f2;
+  }
   main {
     text-align: center;
     padding: 1em;
