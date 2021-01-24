c7ef35c5755c8e076ca2aad26194283a26fb1f2d
dangelo <dangelomedinag@gmail.com>
2021-01-24 14:11:07 -0300

    fourth commit

---
+++ api/products.js
@@ -0,0 +1,13 @@
+import { db } from '../firestore'
+
+module.exports = (req, res) => {
+  let productosarr = []
+  const response = await db.collection('productos').get();
+    response.forEach((doc) => {
+      const obj = doc.data()
+      productosarr = [...productosarr, {...obj}];
+    });
+
+
+  res.status(200).json(productosarr);
+}
---
+++ firestore.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+import admin from 'firebase-admin'
+dotenv.config()
+
+const adminsdkconfig = {
+  "project_id": process.env.FIRESTORE_project_id ,
+  "private_key": process.env.FIRESTORE_private_key.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIRESTORE_client_email ,
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(adminsdkconfig)
+});
+
+export const db = admin.firestore();
