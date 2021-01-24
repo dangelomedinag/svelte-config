6556d2becb8944edd8012b3e754d0a8d5ac14b6b
dangelo <dangelomedinag@gmail.com>
2021-01-24 14:28:13 -0300

    seventh commit

--- api/products.js
+++ api/products.js
@@ -1,13 +1,18 @@
 const { db } = require('../firestore.js')
 
-module.exports = async(req, res) => {
+module.exports = (req, res) => {
   let productosarr = []
-  const response = await db.collection('productos').get();
-    response.forEach((doc) => {
+  const response = db.collection('productos').get().then(data => {
+    data.forEach((doc) => {
       const obj = doc.data()
       productosarr = [...productosarr, {...obj}];
     });
+    res.status(200).json(productosarr);
+  });
+    // response.forEach((doc) => {
+    //   const obj = doc.data()
+    //   productosarr = [...productosarr, {...obj}];
+    // });
 
 
-  res.status(200).json(productosarr);
 }
