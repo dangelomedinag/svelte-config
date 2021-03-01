fb30dd90117a0675455e6da1242b44c4bfc482c6
dangelo <dangelomedinag@gmail.com>
2021-03-01 00:42:16 -0300

    second test post 10

--- api/create.js
+++ api/create.js
@@ -1,19 +1,21 @@
 const { db } = require('../firestore.js')
 
-module.exports = async (req, res) => {
-  // const {number} = req.body
-
-  // res.status(200).json({res: number * 2});
+module.exports = (req, res) => {
+  
 	const {id} = req.body;
 	
 	let documentRef = db.doc(`productos/${id}`)
-	
-	documentRef.update({likes: 100}).then(response => {
-		res.status(200).json({id, updated_at: response._writeTime._seconds})
+
+	documentRef.get().then(documentSnapshot => {
+  let current = documentSnapshot.data().likes;
+
+		documentSnapshot.ref.update({likes: current + 1}).then(result => {
+			res.status(200).json({id, updated_at: response._writeTime._seconds})
+		})
 	}).catch(err=>{
-		console.log(err)
 		res.status(301).json(JSON.stringify({message: "no se pudo actualizar el documento", err}))
 	})
+	
 }
 
 
