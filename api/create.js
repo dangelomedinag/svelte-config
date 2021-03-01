67095ae71e3a0a8e9a19a0d502099ea28e7796c6
dangelo <dangelomedinag@gmail.com>
2021-02-28 22:47:46 -0300

    second test post 4

--- api/create.js
+++ api/create.js
@@ -1,6 +1,6 @@
 // const { db } = require('../firestore.js')
 
-module.exports = async(req, res) => {
+module.exports = (req, res) => {
   const {number} = req.body
 
   res.status(200).json({res: number * 2});
