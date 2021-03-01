c760e67e68cf6d7312ce1764f8a7f13a9f2fe748
dangelo <dangelomedinag@gmail.com>
2021-02-28 23:00:09 -0300

    second test post 5

--- api/create.js
+++ api/create.js
@@ -5,3 +5,16 @@ module.exports = async (req, res) => {
 
   res.status(200).json({res: number * 2});
 }
+
+router.put('/editar', async(req, res, next) => {
+  const {id} = req.body;
+
+  let documentRef = db.doc(`productos/${id}`)
+  
+  documentRef.update({likes: 100}).then(response => {
+    res.status(200).json({id, updated_at: response._writeTime._seconds})
+  }).catch(err=>{
+    console.log(err)
+    res.status(301).end(JSON.stringify({message: "no se pudo actualizar el documento", err}))
+  })
+})
