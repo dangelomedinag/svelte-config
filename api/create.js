e695c082fe1210c83159a2836196c04ad57282c4
dangelo <64056830+dangelomedinag@users.noreply.github.com>
2021-02-28 22:56:58 -0300

    Update create.js

--- api/create.js
+++ api/create.js
@@ -1,6 +1,6 @@
 // const { db } = require('../firestore.js')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const {number} = req.body
 
   res.status(200).json({res: number * 2});
