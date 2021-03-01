a7ba10e67fe80061fc5d7f3e82d89df8573d37e5
dangelo <dangelomedinag@gmail.com>
2021-02-28 23:03:22 -0300

    second test post 6

--- api/create.js
+++ api/create.js
@@ -1,20 +1,19 @@
 // const { db } = require('../firestore.js')
 
 module.exports = async (req, res) => {
-  const {number} = req.body
+  // const {number} = req.body
 
-  res.status(200).json({res: number * 2});
+  // res.status(200).json({res: number * 2});
+	const {id} = req.body;
+	
+	let documentRef = db.doc(`productos/${id}`)
+	
+	documentRef.update({likes: 100}).then(response => {
+		res.status(200).json({id, updated_at: response._writeTime._seconds})
+	}).catch(err=>{
+		console.log(err)
+		res.status(301).end(JSON.stringify({message: "no se pudo actualizar el documento", err}))
+	})
 }
 
-router.put('/editar', async(req, res, next) => {
-  const {id} = req.body;
 
-  let documentRef = db.doc(`productos/${id}`)
-  
-  documentRef.update({likes: 100}).then(response => {
-    res.status(200).json({id, updated_at: response._writeTime._seconds})
-  }).catch(err=>{
-    console.log(err)
-    res.status(301).end(JSON.stringify({message: "no se pudo actualizar el documento", err}))
-  })
-})
