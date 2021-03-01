2b4865c70aed6f04f975ec519d314f6023b8f068
dangelo <dangelomedinag@gmail.com>
2021-03-01 19:47:52 -0300

    game changed

--- api/create.js
+++ api/create.js
@@ -1,27 +1,7 @@
 const { db } = require('../firestore.js')
 
-// module.exports = allowCors(handler)
-
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another option
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-	const {id} = req.body;
+module.exports = (req, res) => {
+  const {id} = req.body;
 	
 	let documentRef = db.doc(`productos/${id}`)
 
@@ -36,4 +16,36 @@ const handler = (req, res) => {
 	})
 }
 
-module.exports = allowCors(handler)
+
+// const allowCors = fn => async (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   return await fn(req, res)
+// }
+
+// const handler = (req, res) => {
+// 	const {id} = req.body;
+	
+// 	let documentRef = db.doc(`productos/${id}`)
+
+// 	documentRef.get().then(documentSnapshot => {
+//   let current = documentSnapshot.data().likes;
+
+// 		documentSnapshot.ref.update({likes: current + 1}).then(result => {
+// 			res.status(200).json({id, likes: current + 1, updated_at: result._writeTime._seconds}).end()
+// 		})
+// 	}).catch(err=> {
+// 		res.status(301).json(JSON.stringify({message: "no se pudo actualizar el documento", err})).end()
+// 	})
+// }
+
+// module.exports = allowCors(handler)
