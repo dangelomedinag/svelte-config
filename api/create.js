f53a7ea5fb50d0811e5cd3784bcbcded134a531d
dangelo <dangelomedinag@gmail.com>
2021-03-01 13:41:48 -0300

    new format serveless functions

--- api/create.js
+++ api/create.js
@@ -1,6 +1,23 @@
 const { db } = require('../firestore.js')
 
-module.exports = (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+
+
+const handler = (req, res) => {
   
 	const {id} = req.body;
 	
@@ -17,5 +34,7 @@ module.exports = (req, res) => {
 	})
 	 
 }
+
+module.exports = allowCors(handler)
 
 
